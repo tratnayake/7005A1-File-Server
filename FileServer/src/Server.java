b2719a8a688a90ccf264aca65bc1986b9e5a29c4
Thilina Ratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2014-09-16 11:21:48 -0700

    First

---
+++ FileServer/src/Server.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thilinaratnayake
+ */
+public class Server {
+    
+    private static int port= 7005;
+    
+    public Server (int port){
+        this.port = port;
+    }
+    
+    public static void main(String[] args) {
+    }
+    
+    
+}
