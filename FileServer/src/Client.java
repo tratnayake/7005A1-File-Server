abf6c16e6e8c0e334261566e693326d3a7e75d49
Thilina Ratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2014-09-17 10:59:10 -0700

    Test

--- FileServer/src/Client.java
+++ FileServer/src/Client.java
@@ -1,3 +1,11 @@
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +17,36 @@
  * @author thilinaratnayake
  */
 public class Client {
-    
+    public static void main(String[] args){
+        try {
+			Socket socket = new Socket("127.0.0.1", 8005);
+		} 
+		// if it failed not much I can so
+		catch(Exception ec) {
+                        System.out.println("Error connectiong to server:" + ec);
+			
+		}
+		Socket socket;
+        try {
+            socket = new Socket("127.0.0.1", 8005);
+            
+		String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+		      System.out.println(msg);
+                      ObjectOutputStream sOutput;
+                   ObjectInputStream sInput;
+                   
+			sInput  = new ObjectInputStream(socket.getInputStream());
+			sOutput = new ObjectOutputStream(socket.getOutputStream());
+                        System.out.println("WRiting test");
+                        sOutput.writeObject("Test");
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+	
+		
+
+		
+		
+    }
 }
--- FileServer/src/Server.java
+++ FileServer/src/Server.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.net.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,13 +15,56 @@ import java.net.*;
  */
 public class Server {
     
-    private static int port= 7005;
+    private static int port= 8005;
+    private static boolean started = true;
     
     public Server (int port){
         this.port = port;
     }
     
     public static void main(String[] args) {
+        //Create the new server on port 7005
+        System.out.println("Start @ Main");
+        Server server1 = new Server(8005);  
+        //Start the server
+        System.out.println("serverStart");
+        server1.serverStart();
+        
+    }
+    
+    public static void serverStart(){
+                    
+                   ObjectOutputStream sOutput;
+                   ObjectInputStream sInput;
+                    
+        try {
+            ServerSocket servsocket = new ServerSocket(port);
+            
+            while(started){
+                Socket socket = servsocket.accept();
+                
+                try{
+                    System.out.println("Connection Accepted!");
+                    
+                    // create output first
+				sOutput = new ObjectOutputStream(socket.getOutputStream());
+				sInput  = new ObjectInputStream(socket.getInputStream());
+				// read the username
+                                String input = (String) sInput.readObject();
+                                System.out.println(input);
+				
+                }
+                
+                catch (Exception e){
+                    System.out.println("Exception is " +e);
+                }
+            }
+        }
+        
+        catch(IOException e){
+            String msg = " Exception on new ServerSocket: " + e + "\n";
+			         System.out.println(msg);
+        }
     }
     
     
