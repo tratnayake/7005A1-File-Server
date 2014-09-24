447626ed63f63361db6015bd61ddb8a82b312bd8
Thilina Ratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2014-09-24 12:52:38 -0700

    From work at school

--- FileServer/src/Client.java
+++ FileServer/src/Client.java
@@ -1,15 +1,8 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.io.*;
-import java.net.*;
  
 
 /*
@@ -23,42 +16,42 @@ import java.net.*;
  * @author thilinaratnayake
  */
 public class Client {
-    public static void main(String[] args){
-        try {
-			//Socket socket = new Socket("127.0.0.1", 8005);
-		} 
-		// if it failed not much I can so
-		catch(Exception ec) {
-                        System.out.println("Error connectiong to server:" + ec);
-			
-		}
-	 try (
-                 //Create a new socket
-            Socket echoSocket = new Socket("127.0.0.1", 8005);
-            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
-            ) 
-         {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                System.out.println("Entered in: "+userInput);
-                out.println(userInput);
-                System.out.println("Finished writing:" +userInput);
-                //System.out.println("echo: " + in.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + "127.0.0.1");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-               "127.0.01");
-            System.exit(1);
-        } 
-	
-		
-
-		
-		
+    public static void main(String[] args) throws Exception{
+        Socket clientSock = new Socket("127.0.0.1", 8006);
+        File myFile = new File("send.txt");
+        
+        
+           
+                     
+        
     }
+    
+    public void SEND(File file, Socket outgoingSocket)
+    {
+        BufferedInputStream bis = null;
+        try {
+            File myFile = file;
+            Socket clientSock = outgoingSocket;
+            byte[] mybytearray = new byte[(int) myFile.length()];
+            bis = new BufferedInputStream(new FileInputStream(myFile));
+            bis.read(mybytearray, 0, mybytearray.length);
+            System.out.println("Works until here");
+            OutputStream os = clientSock.getOutputStream();
+            os.write(mybytearray, 0, mybytearray.length);
+            os.flush();
+            clientSock.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                bis.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+    
 }
--- FileServer/src/Server.java
+++ FileServer/src/Server.java
@@ -3,6 +3,8 @@ import java.net.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,8 +17,9 @@ import java.util.*;
  */
 public class Server {
     
-    private static int port= 8005;
+    private static int port= 8006;
     private static boolean started = true;
+    File fileToSend = new File("alice.txt");
     
     public Server (int port){
         this.port = port;
@@ -25,7 +28,7 @@ public class Server {
     public static void main(String[] args) {
         //Create the new server on port 7005
         System.out.println("Start @ Main");
-        Server server1 = new Server(8005);  
+        Server server1 = new Server(8006);  
         //Start the server
         System.out.println("serverStart");
         server1.serverStart();
@@ -47,35 +50,17 @@ public class Server {
                 try{
                     System.out.println("Connection Accepted!");
                     
-                    // create output first
-				PrintWriter sOutput = new PrintWriter(socket.getOutputStream(),true);
-				BufferedReader sInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                                BufferedReader userInput=(new BufferedReader(new InputStreamReader(System.in)));
-                                
-                                InputStream stream = socket.getInputStream();
-                                byte[] buffer = new byte[80];
-                                
-                                
-				// read the username
-                              
-                               //String inputLine;
-                               //System.out.println(input);
-                                
-//                                while (stream.read(buffer)> 0){
-//                                    System.out.println((new BufferedReader(new InputStreamReader(stream))).readLine());
-//                                }
-                     String test;           
-                                
-            while ((test = sInput.readLine()) != null) {
-                System.out.println("Not Null");
-                
-               
-                //System.out.println(input.readLine());
-                System.out.println(test);
-                System.out.println("End Sandwich");
-                
-                
-            }
+                    //Choose what Client wants server to do, Either go into GET or SEND
+                    Server.ChooseMode(socket);
+                    
+                    
+                    
+                    
+                    System.out.println("Starting GET");
+                    Server.GET(socket);
+                    
+                    
+                    
 //            
                                 
 				
@@ -92,6 +77,113 @@ public class Server {
 			         System.out.println(msg);
         }
     }
+    
+    
+    
+    public static void ChooseMode(Socket socket){
+        
+        try {
+            //Get Input from Stream
+           
+            
+           BufferedReader sInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            String decision;
+            
+            while ((decision= sInput.readLine()) != null) {
+                System.out.println(decision);
+               
+            }             
+                                
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        
+    }
+    
+    public static void GET(Socket incomingSocket){
+        System.out.println("Start Get");
+        //Choose your incoming socket
+       Socket socket = incomingSocket;
+        //Define buffer size
+       byte[] bufferByteArray = new byte[1048];
+        
+        try {
+            // Get the inputstream from socket
+            InputStream is = socket.getInputStream();
+            // Read the number of bytes that need to be read.
+            int fileSizeInBits = is.read(bufferByteArray, 0, bufferByteArray.length);
+            System.out.println(fileSizeInBits);
+            
+            // Create a new file output stream to handle writing the file onto the server.
+            FileOutputStream foutput = new FileOutputStream("ReceivedFile.txt");
+            // Use a buffered output stream that writes to the file output stream
+            BufferedOutputStream boutput = new BufferedOutputStream(foutput);
+            
+            //Write the file
+            boutput.write(bufferByteArray, 0, fileSizeInBits);
+            
+            
+           //CLose the buffer
+            boutput.close();
+           //Close the socket
+            System.out.println("Before close");
+            socket.close();
+  
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+    public static void SEND(File fileToSend, Socket socket){
+        while (true){
+            
+            //Create a new buffer array to hold the file
+            byte[] bufferByteArray = new byte[(int) fileToSend.length()];
+            
+            try {
+                //Create a file input stream to take in the file
+                FileInputStream fInput = new FileInputStream(fileToSend);
+                
+                //Create a buffered input stream to read that file input stream
+                BufferedInputStream bInput = new BufferedInputStream(fInput);
+                
+                //Read the file through the buffered input stream until the length of the file
+                bInput.read(bufferByteArray, 0,bufferByteArray.length);
+                
+                //Make an output stream to write the file to the socket
+                OutputStream os = socket.getOutputStream();
+                
+                //Write everything in buffer to outputstream, until the size of the outputstream
+                os.write(bufferByteArray, 0, bufferByteArray.length);
+                
+                System.out.println("File Written");
+                
+                //flush the output stream
+                os.flush();
+                
+                //Close the socket
+                socket.close();
+                
+                
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+        
+    
+}
     
     
 }
